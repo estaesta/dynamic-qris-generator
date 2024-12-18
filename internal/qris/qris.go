a0233b79022a461d741b157509889da0c72eaefc
estaesta <ahmadforesta@gmail.com>
2024-12-18 18:17:31 +0700

    add parse qris

--- internal/qris/qris.go
+++ internal/qris/qris.go
@@ -6,6 +6,7 @@ import (
 	_ "image/jpeg"
 	_ "image/png"
 	"io"
+	"strings"
 
 	"github.com/makiuchi-d/gozxing"
 	_ "github.com/makiuchi-d/gozxing"
@@ -41,9 +42,34 @@ func validateQrisData(qrisData string) bool {
 	return true
 }
 
+type QrisData struct {
+	Version                    string // 00
+	Type                       string // 01
+	MerchantAccountInformation string // 26
+	MerchantCategoryCode       string // 52
+	TransactionCurrency        string // 53
+	CountryCode                string // 58
+	MerchantName               string // 59
+	MerchantCity               string // 60
+	PostalCode                 string // 61
+	MerchantAdditionalData     string // 62
+	Crc                        string // 63
+}
+
+func parseQrisData(qrisString string) QrisData {
+	// Parse QRIS data
+	var qrisData = QrisData{}
+
+	// find between "ID59" and "60"
+	qrisData.MerchantName = qrisString[strings.Index(qrisString, "ID59")+4 : strings.Index(qrisString, "60")]
+
+	return qrisData
+}
+
 // A function to convert QRIS data to dynamic QRIS data
-func convertQrisDataToDynamic(qrisData *string) {
+func convertQrisDataToDynamic(qrisData *string) error {
 	// Convert QRIS data
+	return nil
 }
 
 // A function to generate QRIS image from QRIS data string
